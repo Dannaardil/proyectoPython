94098860e6c7c6e96b3a9fd230778b06bef20b00
helen murillo <hnmurillo24@gmail.com>
2024-02-03 10:10:01 -0500

    feat: :sparkles: Se realizo el menu de inicio

---
+++ main.py
@@ -0,0 +1,32 @@
+from os import system
+import module.camper as camper 
+
+
+def menu():
+    print("""
+          BIENVENIDO A... 
+          
+
+          ______________________________________
+          |                                     |
+          |>>Seguimiento Academico CampusLands<<|
+          |_____________________________________|
+          
+          """)
+    print("\t1. Info CAMPER ")
+    print("\t2. Info TRAINER ")
+    print("\t3. Registros ")
+    print("\t4. Modulo de reportes")
+
+def save (): 
+    system ("clear")
+    print("""  
+      ______________________
+     /                     /
+     /    ADMIN CAMPER     /
+     /_____________________/
+
+
+
+""")
+
